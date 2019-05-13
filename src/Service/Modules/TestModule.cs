dbbf4af1663a3285b89c49ec602c5a9a16846a5d
Andrew Fraser <andrewfraser73@hotmail.com>
2019-05-13 14:00:41 +0100

    test module for checking

---
+++ src/Service/Modules/TestModule.cs
@@ -0,0 +1,17 @@
+﻿namespace Linn.Authorisation.Service.Modules
+{
+    using System;
+
+    public class TestModule : Carter.CarterModule
+    {
+        public TestModule()
+        {
+            this.Get("/authorisation/test", this.Test);
+        }
+
+        private async System.Threading.Tasks.Task Test(Microsoft.AspNetCore.Http.HttpRequest req, Microsoft.AspNetCore.Http.HttpResponse res, Microsoft.AspNetCore.Routing.RouteData routeData)
+        {
+            throw new Exception("Will this be logged");
+        }
+    }
+}
