ddacd1203735e9e01205b5d88f2331bc2c0f86ed
Peter <Peter.MacDonough@linn.co.uk>
2019-05-14 16:30:32 +0100

    Update test script file to point at new tests

--- scripts/test.sh
+++ scripts/test.sh
@@ -12,9 +12,8 @@ fi
 # c# tests
 dotnet test ./tests/Unit/Domain.Tests/Domain.Tests.csproj
 dotnet test ./tests/Unit/Facade.Tests/Facade.Tests.csproj
-# dotnet test ./tests/Unit/Messaging.Tests/Messaging.Tests.csproj
 dotnet test ./tests/Unit/Proxy.Tests/Proxy.Tests.csproj
-dotnet test ./tests/Integration/Service.Tests/Service.Tests.csproj
+dotnet test ./tests/Integration/Integration.Tests/Integration.Tests.csproj
 
 echo $?
 if [ $? -eq 1 ]; then
