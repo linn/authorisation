88530a4a40841c2245a9f955cf05bc601684dfba
lewisrenfrew <Lewis.Renfrew@linn.co.uk>
2024-01-12 18:04:41 +0000

    include group members so group permissions dont disappear (#75)

--- src/IoC/PersistenceExtensions.cs
+++ src/IoC/PersistenceExtensions.cs
@@ -2,10 +2,10 @@
 {
     using Linn.Authorisation.Domain;
     using Linn.Authorisation.Domain.Groups;
-    using Linn.Common.Persistence;
-    using Linn.Common.Persistence.EntityFramework;
     using Linn.Authorisation.Domain.Permissions;
     using Linn.Authorisation.Persistence;
+    using Linn.Common.Persistence;
+    using Linn.Common.Persistence.EntityFramework;
 
     using Microsoft.EntityFrameworkCore;
     using Microsoft.Extensions.DependencyInjection;
@@ -14,14 +14,13 @@
     {
         public static IServiceCollection AddPersistence(this IServiceCollection services)
         {
-            return services.AddScoped<ServiceDbContext>()
-                .AddTransient<DbContext>(a => a.GetService<ServiceDbContext>())
+            return services.AddScoped<ServiceDbContext>().AddTransient<DbContext>(a => a.GetService<ServiceDbContext>())
                 .AddTransient<ITransactionManager, TransactionManager>()
-                .AddTransient<IRepository<Privilege, int>, EntityFrameworkRepository<Privilege, int>>(r =>
-                    new EntityFrameworkRepository<Privilege, int>(r.GetService<ServiceDbContext>()?.Privileges))
+                .AddTransient<IRepository<Privilege, int>, EntityFrameworkRepository<Privilege, int>>(
+                    r => new EntityFrameworkRepository<Privilege, int>(r.GetService<ServiceDbContext>()?.Privileges))
                 .AddTransient<IRepository<Permission, int>, PermissionsRepository>()
-                .AddTransient<IRepository<Group, int>, EntityFrameworkRepository<Group, int>>(r =>
-                    new EntityFrameworkRepository<Group, int>(r.GetService<ServiceDbContext>()?.Groups));
+                .AddTransient<IRepository<Group, int>, GroupRepository>();
+
         }
     }
 }
