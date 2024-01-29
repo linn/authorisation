d5b5d84523254a25224b47355e2a04b5f58a6a83
Lewis Renfrew <lewis.renfrew@linn.co.uk>
2024-01-29 10:30:31 +0000

    GetById integration test

---
+++ tests/Integration/Integration.Tests/PrivilegeModuleTests/WhenGettingById.cs
@@ -0,0 +1,51 @@
+﻿namespace Linn.Authorisation.Integration.Tests.PrivilegeModuleTests
+{
+    using System.Net;
+
+    using FluentAssertions;
+
+    using Linn.Authorisation.Domain;
+    using Linn.Authorisation.Integration.Tests.Extensions;
+    using Linn.Authorisation.Resources;
+
+    using NSubstitute;
+
+    using NUnit.Framework;
+
+    public class WhenGettingById : ContextBase
+    {
+        [SetUp]
+        public void SetUp()
+        {
+            this.PrivilegeRepository.FindById(1).Returns(new Privilege { Id = 1, Name = "name" });
+
+            this.Response = this.Client.Get(
+                "/authorisation/privileges/1",
+                with =>
+                    {
+                        with.Accept("application/json");
+                    }).Result;
+        }
+
+        [Test]
+        public void ShouldReturnOk()
+        {
+            this.Response.StatusCode.Should().Be(HttpStatusCode.OK);
+        }
+
+        [Test]
+        public void ShouldReturnJsonContentType()
+        {
+            this.Response.Content.Headers.ContentType.Should().NotBeNull();
+            this.Response.Content.Headers.ContentType?.ToString().Should().Be("application/json");
+        }
+
+        [Test]
+        public void ShouldReturnJsonBody()
+        {
+            var resources = this.Response.DeserializeBody<PrivilegeResource>();
+            resources.Id.Should().Be(1);
+            resources.Name.Should().Be("name");
+        }
+    }
+}
