63b4de7c11183c343dc2403c9379105cb6991598
Lewis Renfrew <lewis.renfrew@linn.co.uk>
2024-02-01 08:45:06 +0000

    C# PUT endpoint test

---
+++ tests/Integration/Integration.Tests/PrivilegeModuleTests/WhenPutting.cs
@@ -0,0 +1,59 @@
+namespace Linn.Authorisation.Integration.Tests.PrivilegeModuleTests
+{
+    using System.Net;
+    using System.Net.Http.Json;
+
+    using FluentAssertions;
+
+    using Linn.Authorisation.Domain;
+    using Linn.Authorisation.Integration.Tests.Extensions;
+    using Linn.Authorisation.Resources;
+
+    using NSubstitute;
+
+    using NUnit.Framework;
+
+    public class WhenPutting : ContextBase
+    {
+        private PrivilegeResource updatedResource;
+
+        private Privilege current;
+
+        [SetUp]
+        public void SetUp()
+        {
+            this.updatedResource = new PrivilegeResource { Name = "new.name", Active = false, Id = 12} ;
+
+            this.current = new Privilege { Id = 12, Name = "old.name", Active = true };
+            this.PrivilegeRepository.FindById(this.current.Id).Returns(this.current);
+            this.Response = this.Client.PutAsJsonAsync("/authorisation/privileges/12", this.updatedResource).Result;
+        }
+
+        [Test]
+        public void ShouldReturnSuccess()
+        {
+            this.Response.StatusCode.Should().Be(HttpStatusCode.OK);
+        }
+        
+        [Test]
+        public void ShouldReturnJsonContentType()
+        {
+            this.Response.Content.Headers.ContentType.Should().NotBeNull();
+            this.Response.Content.Headers.ContentType?.ToString().Should().Be("application/json");
+        }
+
+        [Test]
+        public void ShouldUpdate()
+        {
+            this.current.Name.Should().Be(this.updatedResource.Name);
+            this.current.Active.Should().Be(this.updatedResource.Active);
+        }
+
+        [Test]
+        public void ShouldReturnUpdated()
+        {
+            var result = this.Response.DeserializeBody<PrivilegeResource>();
+            result.Name.Should().Be(this.updatedResource.Name);
+        }
+    }
+}
