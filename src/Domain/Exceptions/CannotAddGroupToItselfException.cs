c7e8f3f9cbc2251cc4e40dbcff78cd3a36811b0f
Lewis Renfrew <lewis.renfrew@linn.co.uk>
2024-08-02 09:30:40 +0100

    tighten up adding to groups logic to avoid impossible group states

---
+++ src/Domain/Exceptions/CannotAddGroupToItselfException.cs
@@ -0,0 +1,16 @@
+﻿namespace Linn.Authorisation.Domain.Exceptions
+{
+    using System;
+    using Common.Domain.Exceptions;
+
+    public class CannotAddGroupToItselfException : DomainException
+    {
+        public CannotAddGroupToItselfException(string message) : base(message)
+        {
+        }
+
+        public CannotAddGroupToItselfException(string message, Exception innerException) : base(message, innerException)
+        {
+        }
+    }
+}
