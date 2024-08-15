2651f08cc252e6330e2526bce774a06b15d615ad
Lewis Renfrew <lewis.renfrew@linn.co.uk>
2024-08-15 16:29:54 +0100

    actually fix

--- src/Facade/ResourceBuilders/PermissionResourceBuilder.cs
+++ src/Facade/ResourceBuilders/PermissionResourceBuilder.cs
@@ -12,29 +12,39 @@ namespace Linn.Authorisation.Facade.ResourceBuilders
     {
         public object Build(Permission model, IEnumerable<string> claims)
         {
+            if (model is IndividualPermission)
+            {
+                return new PermissionResource
+                           {
+                               GranteeUri = ((IndividualPermission)model).GranteeUri,
+                               Privilege = model.Privilege.Name,
+                               PrivilegeId = model.Privilege.Id,
+                               Links = this.BuildLinks(model).ToArray(),
+                               Id = model.Id
+                           };
+            }
+
             return new PermissionResource
                        {
                            Privilege = model.Privilege.Name,
                            PrivilegeId = model.Privilege.Id,
                            Links = this.BuildLinks(model).ToArray(),
-                           Id = model.Id
+                           Id = model.Id,
+                           GranteeGroupId = ((GroupPermission)model).GranteeGroup.Id,
+                           GroupName = ((GroupPermission)model).GranteeGroup.Name
                        };
         }
-        
+
         public string GetLocation(Permission model)
         {
             return $"/authorisation/permissions/{model.Id}";
         }
-        
+
         object IBuilder<Permission>.Build(Permission model, IEnumerable<string> claims) => this.Build(model, claims);
-        
+
         private IEnumerable<LinkResource> BuildLinks(Permission permission)
         {
-            yield return new LinkResource
-                             {
-                                 Rel = "self",
-                                 Href = $"/permissions/{permission.Id}"
-                             };
+            yield return new LinkResource { Rel = "self", Href = $"/permissions/{permission.Id}" };
         }
     }
 }
