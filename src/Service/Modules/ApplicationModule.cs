b34126885a9df2276105ab4bc2e8f6b0b144c266
OliverMclachlan <146720368+OliverMclachlan@users.noreply.github.com>
2024-08-15 14:32:50 +0100

    Fixing bugs

--- src/Service/Modules/ApplicationModule.cs
+++ src/Service/Modules/ApplicationModule.cs
@@ -16,6 +16,14 @@
         {
             app.MapGet("/", this.Redirect);
             app.MapGet("/authorisation", this.GetApp);
+            app.MapGet("/authorisation/view-permission-users", this.GetApp);
+            app.MapGet("/authorisation/create-individual-permission", this.GetApp);
+            app.MapGet("/authorisation/create-group-permission", this.GetApp);
+            app.MapGet("/authorisation/view-permission-users", this.GetApp);
+            app.MapGet("/authorisation/view-individual-permission", this.GetApp);
+            app.MapGet("/authorisation/groups", this.GetApp);
+            app.MapGet("/authorisation/groups/create", this.GetApp);
+            app.MapGet("/authorisation/groups/add-individual-member", this.GetApp);
         }
 
         private Task Redirect(HttpRequest req, HttpResponse res)
