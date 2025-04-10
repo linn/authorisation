9006f09de2c2ad924674857737e94dbf96e5ee66
OliverMclachlan <OliverM@linn.co.uk>
2025-04-10 14:32:04 +0100

    Adding groupService

---
+++ src/Domain/Services/GroupService.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using Linn.Authorisation.Domain.Groups;
+
+namespace Linn.Authorisation.Domain.Services
+{
+    using System.Collections.Generic;
+    using Linn.Common.Persistence;
+
+    public class GroupService : IGroupService
+    {
+        private readonly IRepository<Group, int> groupRepository;
+
+        public GroupService(IRepository<Group, int> groupRepository)
+        {
+            this.groupRepository = groupRepository;
+        }
+
+        public IEnumerable<Group> GetAllGroupsForUser(IEnumerable<string> userPrivileges = null)
+        {
+            throw new NotImplementedException();
+        }
+
+        public Group GetGroupById(int privilegeId, IEnumerable<string> userPrivileges = null)
+        {
+            throw new NotImplementedException();
+        }
+
+    }
+}
---
+++ src/Domain/Services/IGroupService.cs
@@ -0,0 +1,12 @@
+﻿namespace Linn.Authorisation.Domain.Services
+{
+    using Linn.Authorisation.Domain.Groups;
+    using System.Collections.Generic;
+
+    public interface IGroupService
+    {
+        IEnumerable<Group> GetAllGroupsForUser(IEnumerable<string> privileges = null);
+
+        Group GetGroupById(int groupId, IEnumerable<string> privileges = null);
+    }
+}
