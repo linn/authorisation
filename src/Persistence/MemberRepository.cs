39c642bcf96d68bea14bd30b618dd08e76e172b0
OliverMclachlan <OliverM@linn.co.uk>
2025-05-02 12:18:56 +0100

    feedback

--- src/Persistence/MemberRepository.cs
+++ src/Persistence/MemberRepository.cs
@@ -6,44 +6,15 @@
 
     using Linn.Authorisation.Domain.Groups;
     using Linn.Common.Persistence;
+    using Linn.Common.Persistence.EntityFramework;
 
-    public class MemberRepository : IRepository<Member, int>
+    public class MemberRepository : EntityFrameworkRepository<Member, int>
     {
         private readonly ServiceDbContext serviceDbContext;
 
-        public MemberRepository(ServiceDbContext serviceDbContext)
+        public MemberRepository(ServiceDbContext serviceDbContext) : base(serviceDbContext.Members)
         {
             this.serviceDbContext = serviceDbContext;
-        }
-
-        public Member FindById(int id)
-        {
-            return this.serviceDbContext.Members.SingleOrDefault(p => p.Id == id);
-        }
-
-        public IQueryable<Member> FindAll()
-        {
-            return this.serviceDbContext.Members.OrderBy(p => p.Id);
-        }
-
-        public void Add(Member privilege)
-        {
-            this.serviceDbContext.Members.Add(privilege);
-        }
-
-        public void Remove(Member privilege)
-        {
-            this.serviceDbContext.Members.Remove(privilege);
-        }
-
-        public Member FindBy(Expression<Func<Member, bool>> expression)
-        {
-            return this.serviceDbContext.Members.SingleOrDefault(expression);
-        }
-
-        public IQueryable<Member> FilterBy(Expression<Func<Member, bool>> expression)
-        {
-            return this.serviceDbContext.Members.Where(expression);
         }
     }
 }
