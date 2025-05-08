c53cba5ddc56d734d35d151baaed51b465afdbc8
OliverMclachlan <OliverM@linn.co.uk>
2025-05-08 15:04:09 +0100

    put to old name

--- src/Domain/AuthorisedAction.cs
+++ src/Domain/AuthorisedAction.cs
@@ -2,6 +2,6 @@
 {
     public class AuthorisedAction
     {
-        public const string AuthorisationAuthManager = "authorisation.auth-manager";
+        public const string AuthorisationAuthManager = "authorisation.super-user";
     }
 }
