8337947af8fe1e67d03cc1fac3a904fc1fa9b2e0
OliverMclachlan <OliverM@linn.co.uk>
2025-10-06 11:21:48 +0100

    add visability

--- src/Facade/ResourceBuilders/PermissionResourceBuilder.cs
+++ src/Facade/ResourceBuilders/PermissionResourceBuilder.cs
@@ -18,6 +18,7 @@ namespace Linn.Authorisation.Facade.ResourceBuilders
                 {
                     GranteeUri = ((IndividualPermission)model).GranteeUri,
                     Privilege = model.Privilege.Name,
+                    PrivilegeActive = model.Privilege.Active,
                     PrivilegeId = model.Privilege.Id,
                     Links = this.BuildLinks(model).ToArray(),
                     Id = model.Id,
@@ -30,6 +31,7 @@ namespace Linn.Authorisation.Facade.ResourceBuilders
             {
                 Privilege = model.Privilege.Name,
                 PrivilegeId = model.Privilege.Id,
+                PrivilegeActive = model.Privilege.Active,
                 Links = this.BuildLinks(model).ToArray(),
                 Id = model.Id,
                 GranteeGroupId = ((GroupPermission)model).GranteeGroup.Id,
